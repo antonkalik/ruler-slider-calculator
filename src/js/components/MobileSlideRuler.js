8e1ba5401d4eb29c48789bd49b2fca181b652fad
antonkalik <antonkalik@gmail.com>
2020-08-07 15:15:50 +0300

    mobile refactor

--- src/js/components/MobileSlideRuler.js
+++ src/js/components/MobileSlideRuler.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Wheel from './Wheel';
 
-export default function MobileSlideRuler({ title, ...rest }) {
+export default function MobileSlideRuler({ title, extension, initValue, onChange, ...rest }) {
+  const [value, setValue] = useState(initValue);
 
   return (
     <div className="mobile-slide-ruler">
-      <p className="title">{title}</p>
-      <Wheel {...rest} />
+      <p className="title">
+        {title}: {value} {extension}
+      </p>
+      <Wheel
+        initValue={initValue}
+        extension={extension}
+        onChange={(data) => {
+          setValue(data.value);
+          onChange(data);
+        }}
+        {...rest}
+      />
     </div>
   );
 }
